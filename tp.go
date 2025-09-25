09a1b083e0fbbd562fd3773a0ba6e5f331bd151f
enzoandria1-oss <enzoandria1@gmail.com>
2025-09-25 16:19:52 +0100

    feat(tp.go): :sparkles: new pokemon added "gateau"

--- tp.go
+++ tp.go
@@ -14,9 +14,11 @@ func main() {
 	bulbasaur := Pokemon{Name: "Bulbasaur", Type: "Grass/Poison", Level: 15, Price: 300}
 	charmander := Pokemon{Name: "Charmander", Type: "Fire", Level: 18, Price: 500}
 	lilou := Pokemon{Name: "Lilou", Type: "NRV", Level: 99, Price: 1000}
+	Gateau := Pokemon{Name: "Gateau", Type: "Sucre", Level: 1, Price: 1}
 
 	fmt.Println(pikachu)
 	fmt.Println(bulbasaur)
 	fmt.Println(charmander)
 	fmt.Println(lilou)
+	fmt.Println(Gateau)
 }
