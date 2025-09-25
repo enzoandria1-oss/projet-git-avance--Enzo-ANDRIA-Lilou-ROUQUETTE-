e8267d3deabad8815930b74da1cbf133b6a4104c
enzoandria1-oss <enzoandria1@gmail.com>
2025-09-25 16:15:22 +0100

    feat(tp.go): :sparkles: New pokemon named Lilou

--- tp.go
+++ tp.go
@@ -12,8 +12,10 @@ func main() {
 	pikachu := Pokemon{Name: "Pikachu", Type: "Electric", Level: 25}
 	bulbasaur := Pokemon{Name: "Bulbasaur", Type: "Grass/Poison", Level: 15}
 	charmander := Pokemon{Name: "Charmander", Type: "Fire", Level: 18}
+	lilou := Pokemon{Name: "Lilou", Type: "NRV", Level: 99}
 
 	fmt.Println(pikachu)
 	fmt.Println(bulbasaur)
 	fmt.Println(charmander)
+	fmt.Println(lilou)
 }
