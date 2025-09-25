f0ac8138ac5f8672258f9c5dfe8639343430c230
rlilou <lilou.rouquette@ynov.com>
2025-09-25 16:00:23 +0200

    feat: :sparkles: add class in struct

--- main.go
+++ main.go
@@ -5,19 +5,21 @@ import ("fmt")
 type Name struct{
 	Name string
 	Age int
+	Class string
 }
 
 
 func main () {
-	enzo := Initname("enzo",17)
-	lilou := Initname("lilou",18)
+	enzo := Initname("enzo","B1C",17)
+	lilou := Initname("lilou","B1C",18)
 	fmt.Println(enzo)
 	fmt.Println(lilou)
 }
 
-func Initname (name string, age int)Name{
+func Initname (name, class string, age int)Name{
 	return Name {
 		Name: name,
 		Age: age,
+		Class: class,
 	}
 }
